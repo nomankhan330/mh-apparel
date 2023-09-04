8827569b2443b7fa4313b3fe06bdd4c72f624e58
nomankhan330 <noman.khan330@gmail.com>
2023-09-04 15:15:15 +0500

    redirection change and db update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
             if (Auth::attempt($request->only(["email", "password"]))) {
                 return response()->json([
                     "status" => true,
-                    "redirect" => url("admin/dashboard")
+                    "redirect" => url("admin/categories")
                 ]);
             } else {
                 return response()->json([
