19be42def8a5b9223a51e99fd37f3bf720d9a9d5
nomankhan330 <noman.khan330@gmail.com>
2023-09-11 17:06:16 +0500

    Update WebController.php

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -51,14 +51,14 @@ class WebController extends Controller
             return redirect()->route('login');
         }
 
-        $products = Product::with('category')->with('images')->where('slug', $slug)->first()->toSql();
+        $products = Product::with('category')->with('images')->where('slug', $slug)->first();
         //dd($products->images[0]->image_url);
         //$other_products = Product::where('category_id', $products->category->id)->where('slug', '!=' , $slug)->dd();
 
-        /*$other_products = Product::where('category_id', $products->category->id)->with(['images' => function ($query) {
+        $other_products = Product::where('category_id', $products->category->id)->with(['images' => function ($query) {
             $query->take(1); // Limit the number of images to one per product
-        }])->where('slug', '!=' , $slug)->dd();*/
-        dd($products);
+        }])->where('slug', '!=' , $slug)->dd();
+        dd($other_products);
 
 
         return view('/web/pages/product-details',['products'=>$products],['other_products'=>$other_products]);
